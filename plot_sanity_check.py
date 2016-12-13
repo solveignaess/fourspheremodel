6c8ef17cb747d695c119df707205c356f05f221a
Chaitanya Chintaluri <ccluri@gmail.com>
2016-12-13 16:30:54 +0100

    missing files for sanity checks

--- plot_sanity_check.py
+++ plot_sanity_check.py
@@ -20,18 +20,20 @@ phi_sphere = I*d*prod / (4*np.pi*sigma_brain*(scalp_rad**2))
 #phi_80 = (13.4*np.exp(-0.10*theta) - 0.155 - (0.0135*theta))*phi_0
 
 phi_20_sanity = np.load(os.path.join('results', 'phi_sanity.npy'))
+phi_20_sanity_98 = np.load(os.path.join('results', 'phi_sanity_98.npy'))
 phi_20_c_sanity = np.load(os.path.join('results', 'phi_c_sanity.npy'))
 
 plt.subplot(111)
 #plt.plot(theta, phi_20, 'k', label='Nunez 20')
 
 plt.plot(theta, phi_sphere,  'r', label='Sphere')
-plt.plot(theta, phi_20_sanity, 'g+', label='N&S,2006')
+plt.plot(theta, phi_20_sanity, 'g+', label='Srinivasan-06')
+plt.plot(theta, phi_20_sanity_98, 'k*', label='Srinivasan-98')
 plt.plot(theta, phi_20_c_sanity, 'b.', label='Correction')
 
 plt.xlabel('Polar angle (degrees)')
 plt.ylabel('Potential $\mu V$')
 plt.title('$\sigma_{skull} = \sigma_{brain} = \sigma_{csf} = \sigma_{scalp}$')
 plt.legend()
-#plt.savefig(os.path.join('results', 'Potentials_20.png'))
-plt.show()
+plt.savefig(os.path.join('results', 'sanity.png'), dpi=300)
+#plt.show()
