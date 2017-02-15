e1deea022b3c40c9746f857e1d1f70f0ef79ea89
Chaitanya Chintaluri <ccluri@gmail.com>
2017-02-15 20:24:02 +0100

    typo fix

--- plot_test.py
+++ plot_test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 analy = np.load('./results/Analytical_correct_all.npz')
 calc_ = np.load('./results/CalcPotential4_correct_all.npz')
 
-for key_val in anal.keys():
+for key_val in analy.keys():
     plt.close('all')
     plt.subplot(121)
     plt.imshow(analy[key_val].reshape(180, 180), cmap=plt.cm.PRGn)
